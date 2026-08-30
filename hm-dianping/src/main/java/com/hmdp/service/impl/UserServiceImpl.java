5ad6cd7b2e5a0d4b68e25e54dac8bde5d3d005b1
WZL <18674826785@163.com>
2026-08-30 11:34:39 +0800

    修改session为redis存储

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,19 +1,28 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.PasswordEncoder;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -27,6 +36,9 @@ import java.time.LocalDateTime;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public Result sendCode(String phone, HttpSession session) {
         //检验手机号码
@@ -38,8 +50,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //生成验证码
         String otp= RandomUtil.randomString(4);
 
-        //设置session
-        session.setAttribute("One-Time password",otp);
+//        //设置session
+//        session.setAttribute("One-Time password",otp);
+        //设置redis
+        stringRedisTemplate.opsForValue().set(RedisConstants.LOGIN_CODE_KEY +phone,otp,RedisConstants.LOGIN_CODE_TTL, TimeUnit.MINUTES);
+
         //发送验证码
         log.info("\n验证码为{},{}",otp,otp.hashCode());
 
@@ -55,10 +70,15 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("电话格式错误");
         }
         //解析验证码
-        Object otp = session.getAttribute("One-Time password");
+       /* Object otp = session.getAttribute("One-Time password");
         String otpUser=loginForm.getCode();
         if(session==null||!otpUser.equals(otp.toString())){
             return Result.fail("验证码输入错误");
+        }*/
+        String otp = stringRedisTemplate.opsForValue().get(RedisConstants.LOGIN_CODE_KEY + loginForm.getPhone());
+        String otpUser = loginForm.getCode();
+        if(otp==null||!otpUser.equals(otp)){
+            return Result.fail("验证码错误");
         }
 
         //查表，如果是新用户，新增用户
@@ -73,10 +93,21 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             save(user);
 
         }
-        //设置用户session
+       /* //设置用户session
         session.setAttribute("user",user);
+*/
+        UUID uuid = UUID.randomUUID();
+        String token=RedisConstants.LOGIN_USER_KEY+uuid;
+        UserDTO userDTO=new UserDTO();
+        BeanUtil.copyProperties(user,userDTO);
+        Map<String,Object> map= new HashMap<>();
+        map.put("icon",userDTO.getIcon());
+        map.put("id",userDTO.getId().toString());
+        map.put("nickName",userDTO.getNickName());
 
+        stringRedisTemplate.opsForHash().putAll(token,map);
+        stringRedisTemplate.expire(token,RedisConstants.LOGIN_USER_TTL,TimeUnit.SECONDS);
 
-        return Result.ok();
+        return Result.ok(token);
     }
 }
--- hm-dianping/src/main/java/com/hmdp/utils/LoginIntercepter.java
+++ hm-dianping/src/main/java/com/hmdp/utils/LoginIntercepter.java
@@ -1,20 +1,31 @@
 package com.hmdp.utils;
 
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class LoginIntercepter implements HandlerInterceptor {
 
+    private StringRedisTemplate stringRedisTemplate;
+
+    public LoginIntercepter(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        //对比session,如果不对，拦截
+     /*   //对比session,如果不对，拦截
         HttpSession session=request.getSession();
 
         User user = (User)session.getAttribute("user");
@@ -26,6 +37,26 @@ public class LoginIntercepter implements HandlerInterceptor {
         BeanUtils.copyProperties(user,userDTO);
         UserHolder.saveUser(userDTO);
 
+        return true;*/
+        String token=request.getHeader("authorization");
+
+        if(StrUtil.isBlank(token)){
+            response.setStatus(401);
+            return false;
+        }
+
+        Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(token);
+
+        if(entries==null){
+            response.setStatus(401);
+            return false;
+        }
+
+        UserDTO userDTO=BeanUtil.fillBeanWithMap(entries,new UserDTO(), false);
+
+        UserHolder.saveUser(userDTO);
+
+        stringRedisTemplate.expire(token,RedisConstants.LOGIN_USER_TTL, TimeUnit.SECONDS);
         return true;
     }
 
